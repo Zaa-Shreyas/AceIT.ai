955239200b8715cb043716ae416486acee0f1023
Zaa-Shreyas <shreyas262004@gmail.com>
2025-06-29 17:32:07 +0530

    Landing Page Added to the website

---
+++ frontend/src/utils/data.js
@@ -0,0 +1,29 @@
+
+
+export const APP_FEATURES = [
+    {
+        id: "01",
+        title: "Customizable Prompts",
+        description:"lorem ipsum"
+    },
+    {
+        id: "02",
+        title: "Customizable Prompts",
+        description:"lorem ipsum"
+    },
+    {
+        id: "03",
+        title: "Customizable Prompts",
+        description:"lorem ipsum"
+    },
+    {
+        id: "04",
+        title: "Customizable Prompts",
+        description:"lorem ipsum"
+    },
+    {
+        id: "05",
+        title: "Customizable Prompts",
+        description:"lorem ipsum"
+    },
+];
